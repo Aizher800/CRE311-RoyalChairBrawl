1bdc7f9c11eadb943b8374c74504d6b59b005545
moofrog <60612224+moofrog@users.noreply.github.com>
2020-04-28 18:43:03 +0800

    HealthBar+Script
    A health bar with script, plus a player health script. Once added to the scene, they will need customising.

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    //N.B. we will need to hide the health bar during the pause menu, but keep it's value for when unpaused.
+
+    public Slider slider;
+    public Gradient gradient;   //GRADIENT - To change the colour of the bar according to hp remaining.
+    public Image fill;          //GRADIENT
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;  //sets slider to start at max health
+
+       fill.color = gradient.Evaluate(1f); //GRADIENT - Max health colour, green.
+    }
+
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue); //GRADIENT = normalised, so it goes between 0 & 1.
+    }
+  
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+    
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar; //Ref to HealthBar script.
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+        healthBar.SetMaxHealth(maxHealth);  //Setting up health bar.
+    }
+
+    
+    void Update()
+    {
+        //Attacks - we can change the key codes to whatever is suitable.
+        if(Input.GetKeyDown(KeyCode.Q)) //trigger for damage - Needs to change to collision with other players weapons etc.
+        {
+            TakeDamage(2); //light 
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(5);  //heavy
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            TakeDamage(10); //super
+        }
+    }
+
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth); //Adjusting health bar according to health.
+    }
+}
