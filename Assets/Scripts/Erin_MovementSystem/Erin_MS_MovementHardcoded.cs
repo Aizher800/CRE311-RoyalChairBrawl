12bdd66af8fa69080021104f7dbcd0a4c1b6f7bb
moofrog <60612224+moofrog@users.noreply.github.com>
2020-06-07 20:58:41 +0800

    Created MS scripts
    Created scripts based on the tutorial videos on Co-op local multiplayer.

---
+++ Assets/Scripts/Erin_MovementSystem/Erin_MS_MovementHardcoded.cs
@@ -0,0 +1,49 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.InputSystem;
+
+//public class Erin_MS_MovementHardcoded : MonoBehaviour
+//{
+//    [SerializeField]
+//    private float movementSpeed = 5f;
+
+
+//    void Start()
+//    {
+//        Move();
+//    }
+
+
+//    private void Move()
+//    {
+//        var movement = new Vector3();
+
+
+//       // There is a better way of doing this instead of hardcoding.
+//       //if(Keyboard.current.  whatever key  .boolean)
+//        if(Keyboard.current.wKey.isPressed)
+//        {
+//            movement.z += 1;      // z axis is forward and back movement.
+//        }
+
+//        if (Keyboard.current.sKey.isPressed)
+//        {
+//            movement.z -= 1;      // z axis is forward and back movement.
+//        }
+
+//        if (Keyboard.current.aKey.isPressed)
+//        {
+//            movement.x -= 1;      // x axis is left and right movement - all we need.
+//        }
+
+//        if (Keyboard.current.dKey.isPressed)
+//        {
+//            movement.x += 1;      // x axis is left and right movement -  all we need.
+//        }
+
+//        movement.Normalize();   //If we're moving diagonally, it won't go faster.
+
+//        transform.Translate(movement * movementSpeed * Time.deltaTime);
+//    }
+//}
---
+++ Assets/Scripts/Erin_MovementSystem/Erin_MS_MovementWithAsset.cs
@@ -0,0 +1,59 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Erin_MS_MovementWithAsset : MonoBehaviour
+//{
+//    [SerializeField]
+//    private float movementSpeed = 5f;
+
+//    [SerializeField]
+//    private float jumpHeight = 2;
+
+//    private Erin_MS_Controls controls = null;
+
+
+//    private void Awake()
+//    {
+//        controls = new Erin_MS_Controls(); 
+//    }
+
+
+//    private void OnEnable()
+//    {
+//        controls.Player.Enable();       // Player controls enabled whenever player is.
+//    }
+
+
+//    private void OnDisable()
+//    {
+//        controls.Player.Disable();
+//    }
+
+
+//    private void Update()
+//    {
+//        Move();
+//    }
+
+
+//    public void Jump()
+//    {
+//        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+//    }
+
+
+//    private void Move()
+//    {
+//        var movementInput = controls.Player.Movement.ReadValue<Vector2>();  // Whatever the movement is, read it as a vector 2. Hardcoded - no string references.
+
+//        var movement = new Vector3();   // Reads input value.
+//        movement.x = movementInput.x;   // Convert it into the format we want.
+//        movement.z = movementInput.y;   // Convert it into the format we want.
+        
+//        movement.Normalize();   //If we're moving diagonally, it won't go faster.
+
+//        transform.Translate(movement * movementSpeed * Time.deltaTime);
+//    }
+    
+//}
