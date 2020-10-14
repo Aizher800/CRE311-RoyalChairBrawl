bf21934f4bd4a55f558173687b9c6134d4375474
DESKTOP-R296M1M\Matt Kuzich <mattkkq@hotmail.com>
2020-10-14 12:21:21 +0800

    you can now pick stuff up

--- Assets/Scripts/ItemSystem/GroundItem.cs
+++ Assets/Scripts/ItemSystem/GroundItem.cs
@@ -24,10 +24,11 @@ public class GroundItem : MonoBehaviour, IInteractable
     {
         if(held == false) 
         {
-            Debug.Log("ya got me!");
+            
             Matt_CharacterEquipment _charEquip = _owner.GetComponent<Matt_CharacterEquipment>();
-            if (_charEquip.CheckIfItemHeld() == true) {
+            if (_charEquip.CheckIfItemHeld() == false) {
             held = true;
+                Debug.Log("trying to equip");
             _charEquip.EquipItem(this);
             }
             else
