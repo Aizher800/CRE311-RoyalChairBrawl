9165681a19962e9df3dffdf8fc4a1abc5e968637
DESKTOP-R296M1M\Matt Kuzich <mattkkq@hotmail.com>
2020-10-23 13:06:59 +0800

    new dynamic health ui prefabs

---
+++ Assets/TargetGroupScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class TargetGroupScript : MonoBehaviour
+{
+
+    [SerializeField] CinemachineTargetGroup cinemachineTargetGroup;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cinemachineTargetGroup = FindObjectOfType<CinemachineTargetGroup>();
+        cinemachineTargetGroup.AddMember(gameObject.transform, 1f, 1f);
+    }
+
+    // Update is called once per frame
+    private void OnDestroy()
+    {
+        cinemachineTargetGroup.RemoveMember(gameObject.transform);
+    }
+}
