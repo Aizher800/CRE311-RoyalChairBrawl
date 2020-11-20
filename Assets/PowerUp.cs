caab7127b4fb0f1e5ac218fcfbcdb2064c630457
DESKTOP-R296M1M\Matt Kuzich <mattkkq@hotmail.com>
2020-11-20 15:23:03 +0800

    Rewrote powerup system
    WIP

--- Assets/PowerUp.cs
+++ Assets/PowerUp.cs
@@ -2,16 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum PowerUpType
+{
+    HEALING,
+    ENERGY,
+    SPEED,
+    DAMAGE
+
+}
+[System.Serializable]
+public class PowerUpValues
+{
+    //type of powerup
+    [SerializeField] public PowerUpType type;
+
+
+    //Healing value
+    [SerializeField] public int _healingValue;
+
+    //Energy value
+    [SerializeField] public int _energyValue;
+
+    //damage boost
+    [SerializeField] public float _damageBoost;
+
+    //speed boost
+    [SerializeField] public float _speedBoost;
+
+    //boost duration
+    [SerializeField] public float _boostDuration;
+
+}
 public class PowerUp : GroundItem
 {
-
-    [SerializeField] int _healingValue;
-
-    [SerializeField] int _energyValue;
+   public PowerUpValues _powerUpValues;
     public override void Interact(Matt_SM_PlayerStateInfo _owner)
     {
-        _owner.GetComponent<Erin_UI_PlayerHealth>().RemoveHealth(_healingValue, _owner.PSI_inputNum);
-        Destroy(gameObject);
+        _owner.GetComponent<PowerUpManager>().PickUpPowerUp(this);
+      //  _owner.GetComponent<Erin_UI_PlayerHealth>().RemoveHealth(_healingValue, _owner.PSI_inputNum);
+        //Destroy(gameObject);
     }
 
     // Start is called before the first frame update
---
+++ Assets/PowerUpManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PickUpPowerUp(PowerUp powerUp)
+    {
+        switch (powerUp._powerUpValues.type)
+        {
+            case PowerUpType.HEALING:
+
+                GetComponent<Erin_UI_PlayerHealth>().RemoveHealth(powerUp._powerUpValues._healingValue, GetComponent<Matt_SM_PlayerStateInfo>().PSI_inputNum);
+                break;
+            case PowerUpType.ENERGY:
+                break;
+            case PowerUpType.SPEED:
+                break;
+            case PowerUpType.DAMAGE:
+                break;
+            default:
+                break;
+        }
+
+
+    }
+}
